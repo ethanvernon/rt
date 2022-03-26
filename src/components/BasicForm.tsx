4f767682f752918a2f3b35ad3ecc67baeabdcb7a
ethanvernon <ethan.vernon@colorado.edu>
2022-03-26 16:54:01 -0500

    adds error handling and feedback to login/register

--- src/components/BasicForm.tsx
+++ src/components/BasicForm.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { TextField, Button, Box } from "@mui/material";
+import { TextField, Button, Box, Alert, Snackbar } from "@mui/material";
 import {
   getAuth,
   signInWithEmailAndPassword,
@@ -14,6 +14,8 @@ interface BasicFormProps {
 
 // React FN component
 export const BasicForm = ({ title }: BasicFormProps) => {
+  const [open, setOpen] = React.useState(false);
+  const [errorMessage, setErrorMessage] = React.useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
@@ -22,22 +24,44 @@ export const BasicForm = ({ title }: BasicFormProps) => {
 
   const handleAction = () => {
     if (title === "login") {
-      signInWithEmailAndPassword(authentication, email, password).then(
-        (response) => {
+      signInWithEmailAndPassword(authentication, email, password)
+        .then((response) => {
           navigate("/test");
           sessionStorage.setItem("Auth Token", response.user.refreshToken);
-        }
-      );
+        })
+        .catch((error) => {
+          console.log("sign in error: ", error);
+          if (
+            error.code === "auth/user-not-found" ||
+            error.code === "auth/wrong-password"
+          ) {
+            setErrorMessage("wrong email and/or password.");
+            setOpen(true);
+          }
+        });
     } else if (title === "register") {
       if (email.length > 0 && password.length > 0)
-        createUserWithEmailAndPassword(authentication, email, password).then(
-          (response) => {
+        createUserWithEmailAndPassword(authentication, email, password)
+          .then((response) => {
             console.log("response: ", response);
             sessionStorage.setItem("Auth Token", response.user.refreshToken);
             navigate("/test");
-          }
-        );
+          })
+          .catch((error) => {
+            console.log("create user error: ", error);
+            if (error.code === "auth/email-already-in-use") {
+              setErrorMessage("email already in use.");
+            }
+          });
     }
+  };
+
+  const handleClose = (event: any, reason: string) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
   };
 
   return (
@@ -66,6 +90,16 @@ export const BasicForm = ({ title }: BasicFormProps) => {
       <Button onClick={handleAction} variant="outlined">
         {title}
       </Button>
+      <Snackbar
+        open={open}
+        autoHideDuration={6000}
+        onClose={handleClose}
+        anchorOrigin={{ vertical: "top", horizontal: "center" }}
+      >
+        <Alert severity="error" sx={{ width: "100%" }}>
+          {errorMessage}
+        </Alert>
+      </Snackbar>
     </div>
   );
 };
